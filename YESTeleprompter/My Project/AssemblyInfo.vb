6187266d5d8037cec572e92995fbb2848f8941a5
汪恳 <1174843091@qq.com>
2020-12-11 15:06:54 +0800

    更新版本号

--- YESTeleprompter/My Project/AssemblyInfo.vb
+++ YESTeleprompter/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.2020.0629")>
+<Assembly: AssemblyFileVersion("1.0.2020.1211")>
